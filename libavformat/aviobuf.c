fc04515e5b280fab96fbd9fd71a7475962afa46f
Arnaud Gourdon <agourdon@wyplay.com>
2013-07-10 09:13:59 +0000

    Fix wyrec seeking/speeding. Commit ffmpeg: 7939d0edce490589ce66ccfa21dac0e75a2c2f1c Fix wyrec - Part II

--- libavformat/aviobuf.c
+++ libavformat/aviobuf.c
@@ -221,8 +221,8 @@ int64_t avio_seek(AVIOContext *s, int64_t offset, int whence)
         offset1 >= 0 && offset1 <= (s->buf_end - s->buffer)) {
         /* can do the seek inside the buffer */
         s->buf_ptr = s->buffer + offset1;
-    } else if ((!s->seekable ||
-               offset1 <= s->buf_end + SHORT_SEEK_THRESHOLD - s->buffer) &&
+    } else if (!s->seekable &&
+               offset1 <= s->buf_end + SHORT_SEEK_THRESHOLD - s->buffer &&
                !s->write_flag && offset1 >= 0 &&
                (!s->direct || !s->seek) &&
               (whence != SEEK_END || force)) {

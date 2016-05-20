e29a7d66ddae9d412517b3e3ecdc6a1d9357edb8
vpenne <vpenne@wyplay.com>
2016-05-20 08:52:22 +0000

    add noid3 option to avformat_open_input to skip id3 parsing

--- libavformat/utils.c
+++ libavformat/utils.c
@@ -618,7 +618,7 @@ int avformat_open_input(AVFormatContext **ps, const char *filename, AVInputForma
     }
 
     /* e.g. AVFMT_NOFILE formats will not have a AVIOContext */
-    if (s->pb)
+    if (s->pb && (!options || !*options || !av_dict_get(*options, "noid3", NULL, 0)))
         ff_id3v2_read(s, ID3v2_DEFAULT_MAGIC, &id3v2_extra_meta);
 
     if (!(s->flags&AVFMT_FLAG_PRIV_OPT) && s->iformat->read_header)

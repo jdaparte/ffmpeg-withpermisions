f2f246d193d22d8e7b5b39c1b3f9644e95c6cc88
agourdon <agourdon@wyplay.com>
2019-05-28 09:33:49 +0000

    libavformat/avio_internal.h: Added ffio_read_size method.

--- libavformat/aviobuf.c
+++ libavformat/aviobuf.c
@@ -528,6 +528,14 @@ int avio_read(AVIOContext *s, unsigned char *buf, int size)
     return size1 - size;
 }
 
+int ffio_read_size(AVIOContext *s, unsigned char *buf, int size)
+{
+    int ret = avio_read(s, buf, size);
+    if (ret != size)
+        return AVERROR_INVALIDDATA;
+    return ret;
+}
+
 int ffio_read_partial(AVIOContext *s, unsigned char *buf, int size)
 {
     int len;

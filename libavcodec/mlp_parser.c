FFmpeg/FFmpeg

mlp: Drop ff_ prefix from a static function

libavcodec/mlp_parser.c
@@ -120,7 +120,7 @@ static uint64_t truehd_layout(int chanmap)
     return layout;
 }
 
-static int ff_mlp_get_major_sync_size(const uint8_t * buf, int bufsize)
+static int mlp_get_major_sync_size(const uint8_t * buf, int bufsize)
 {
     int has_extension, extensions = 0;
     int size = 28;
@@ -150,7 +150,7 @@ int ff_mlp_read_major_sync(void *log, MLPHeaderInfo *mh, BitstreamContext *bc)
 
     assert(bitstream_tell(bc) == 0);
 
-    header_size = ff_mlp_get_major_sync_size(bc->buffer, bc->size_in_bits >> 3);
+    header_size = mlp_get_major_sync_size(bc->buffer, bc->size_in_bits >> 3);
     if (header_size < 0 || bc->size_in_bits < header_size << 3) {
         av_log(log, AV_LOG_ERROR, "packet too short, unable to read major sync\n");
         return -1;

jdaparte/ffmpeg-withpermisions

libavcodec/mlp_parser.c
@@ -118,7 +118,7 @@ uint64_t ff_truehd_layout(int chanmap)
     return layout;
 }
 
-static int ff_mlp_get_major_sync_size(const uint8_t * buf, int bufsize)
+static int mlp_get_major_sync_size(const uint8_t * buf, int bufsize)
 {
     int has_extension, extensions = 0;
     int size = 28;
@@ -148,7 +148,7 @@ int ff_mlp_read_major_sync(void *log, MLPHeaderInfo *mh, GetBitContext *gb)
 
     av_assert1(get_bits_count(gb) == 0);
 
-    header_size = ff_mlp_get_major_sync_size(gb->buffer, gb->size_in_bits >> 3);
+    header_size = mlp_get_major_sync_size(gb->buffer, gb->size_in_bits >> 3);
     if (header_size < 0 || gb->size_in_bits < header_size << 3) {
         av_log(log, AV_LOG_ERROR, "packet too short, unable to read major sync\n");
         return -1;